bb27fe48f8d96f8f6c86503ebb1ed3ac2cd94689
guilhermeaj19 <guilhermeadenilson16@gmail.com>
2025-05-08 11:04:45 -0300

    adicionando possíveis atributos

--- medextractor/entities.py
+++ medextractor/entities.py
@@ -4,13 +4,18 @@ from dataclasses import dataclass
 class Medicamento:
     nome: str
     url: str
-    preco: float
+    preco: float #
     code: int
-    marca: str
+    marca: str ##
     categoria: str
     sub_categoria: str
-    principios_ativos: list
+    principios_ativos: list # ##
     image_source: str
-    # is_generico: bool
-    # necessita_prescricao: bool
+    # is_generico: bool #
+    # necessita_prescricao: bool #
+    # necessita_receita: bool
     # descricao: str
+
+    # avaliacao: float #
+    # preco_original: float #
+    # desconto_aplicado: float #
