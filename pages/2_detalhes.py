fbeafc1a17c9bd9ccd5ed238d51ae65e41c73e6f
guilhermeaj19 <guilhermeadenilson16@gmail.com>
2025-07-01 22:17:52 -0300

    Web App: adicionando outros atributos a aba Detalhes

--- pages/2_detalhes.py
+++ pages/2_detalhes.py
@@ -13,19 +13,51 @@ if registro is None:
     st.stop()
 
 with Session() as db:
-    medicamento = db.query(Medicamento).filter_by(registro_ms=registro).first()
-    st.title(medicamento.nome)
-    if medicamento.image_source:
-        st.image(medicamento.image_source, width=150)
+    med = db.query(Medicamento).filter_by(registro_ms=registro).first()
+    st.title(med.nome)
 
-    st.subheader("Ofertas disponíveis:")
+    if med.categoria:
+        categoria = med.categoria
 
-    # 1. Acha o menor preço
-    precos = [oferta.preco for oferta in medicamento.ofertas]
-    menor_preco = min(precos) if precos else None
+        if med.sub_categoria:
+            st.subheader(f"{categoria} -> {med.sub_categoria}")
+        else:
+            st.subheader(f"{categoria}")
 
-    # 2. Mostra ofertas, destacando a mais barata
-    for oferta in medicamento.ofertas:
-        destaque = " - ✅ Melhor preço!" if oferta.preco == menor_preco else ""
-        st.write(f"- **{oferta.farmacia.nome}**: R$ {oferta.preco:.2f}")
-        st.markdown(f"&nbsp;&nbsp;&nbsp;&nbsp;[Ver oferta]({oferta.url}){destaque}")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if med.image_source:
+            st.image(med.image_source, width=300)
+
+    with col2:
+        st.subheader("Ofertas disponíveis:")
+
+        # 1. Acha o menor preço
+        precos = [oferta.preco for oferta in med.ofertas]
+        menor_preco = min(precos) if precos else None
+
+        # 2. Mostra ofertas, destacando a mais barata
+        for oferta in med.ofertas:
+            destaque = " - ✅ Melhor preço!" if oferta.preco == menor_preco else ""
+            st.write(f"- **{oferta.farmacia.nome.capitalize()}**: R$ {oferta.preco:.2f}")
+            st.markdown(f"&nbsp;&nbsp;&nbsp;&nbsp;[Ver oferta]({oferta.url}){destaque}")   
+    
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.subheader("Detalhes:")
+        st.write(f"Registro MS: {med.registro_ms}")
+        if med.marca:
+            st.write(f"Marca: {med.marca}")
+
+        if med.quantidade:
+            st.write(f"Quantidade: {med.quantidade}")
+
+        st.write(f"Genérico? {"✅" if med.is_generico else "❌"}")
+        st.write(f"Necessita prescrição? {"✅" if med.necessita_prescricao else "❌"}")
+        
+    with col2:
+        if med.descricao:
+            st.subheader("Descrição:")
+            st.write(med.descricao)
